1792a8dd2cdd4544520a7be58e5bc41b29851b8d
Eekle <96976531+Eekle@users.noreply.github.com>
2023-10-02 13:46:44 +0200

    Add rust-demangling

--- nesiac/parsing/objects.py
+++ nesiac/parsing/objects.py
@@ -1,13 +1,27 @@
 from dataclasses import dataclass
 import pathlib
 from elftools.elf.elffile import ELFFile, SymbolTableSection
-from cpp_demangle import demangle
+import cpp_demangle
+import rust_demangler
+from rust_demangler.rust import TypeNotFoundError
+import string
 
 @dataclass(frozen=True)
 class ElfObject:
     addr: int
     size: int
     name: str
+
+def _demangle(input: str) -> str:
+    try:
+        rust_d = rust_demangler.demangle(input)
+    except Exception:
+        rust_d = None
+    try:
+        cpp_d = cpp_demangle.demangle(input)
+    except Exception:
+        cpp_d = None
+    return rust_d or cpp_d or input
 
 
 def from_elf(file: pathlib.Path) -> list[ElfObject]:
@@ -19,11 +33,7 @@ def from_elf(file: pathlib.Path) -> list[ElfObject]:
             return outlist
         for sym in symtab.iter_symbols():
             if sym.entry.st_info.type in ["STT_OBJECT", "STT_FUNC"]:
-                try:
-                    name = demangle(sym.name)
-                except ValueError:
-                    name = sym.name
                 outlist.append(
-                    ElfObject(sym.entry.st_value, sym.entry.st_size, name)
+                    ElfObject(sym.entry.st_value, sym.entry.st_size, _demangle(sym.name))
                 )
     return outlist
--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="Nesiac",
-    version="0.2",
+    version="0.3",
     description="Compiled arm binary analysis",
     author="Eekle",
     packages=find_packages(),
     python_requires=">3.10",
     entry_points={"console_scripts": ["nesiac=nesiac.cli:cli"]},
-    install_requires=["rich", "pyelftools", "cpp-demangle"],
+    install_requires=["rich", "pyelftools", "cpp-demangle", "rust_demangler"],
 )
